2d1835f93077d7151d791f14e7565a8f58ad8338
LeeJungHoon <vhrehfdl7tp@naver.com>
2022-02-22 21:59:28 +0900

    Create Fibonacci Modified.py

---
+++ hackerrank/Fibonacci Modified.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'fibonacciModified' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER t1
+#  2. INTEGER t2
+#  3. INTEGER n
+#
+
+def fibonacciModified(t1, t2, n):
+    memo = [0]*(n+1)
+    memo[1], memo[2] = t1, t2
+    for i in range(3, n+1):
+        memo[i] = memo[i-2] + (memo[i-1]**2)
+    return memo[n]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    t1 = int(first_multiple_input[0])
+
+    t2 = int(first_multiple_input[1])
+
+    n = int(first_multiple_input[2])
+
+    result = fibonacciModified(t1, t2, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
